123d420025f49e9f4c8d72b7eee2dabef482a197
Fizzey <alexis.arduca@epitech.eu>
2025-01-17 14:50:33 +0100

    [ADD] - Some scripts and Stairs interaction
    Adding TypeWritterEffect scripts. Create PopUp generic script

---
+++ Assets/Scripts/Managers/GameEventManager.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class GameEventsManager : MonoBehaviour
+{
+    public static GameEventsManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Game Events Manager in the scene.");
+        }
+        instance = this;
+
+        // initialize all events
+    }
+}
---
+++ Assets/Scripts/Utils/MessagePopup.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MessagePopup : MonoBehaviour
+{
+    public string popUpMessage;
+    private GameObject objectPopUp;
+    private TMPro.TMP_Text messagePopUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectPopUp = GameObject.Find("MessagePopUp");
+        objectPopUp.SetActive(false);
+        messagePopUp = objectPopUp.GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (objectPopUp != null)
+            {   
+                messagePopUp.text = popUpMessage;
+
+                objectPopUp.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (objectPopUp != null)
+            {
+                messagePopUp.text = "";
+
+                objectPopUp.SetActive(false);
+            }
+        }
+    }
+}
