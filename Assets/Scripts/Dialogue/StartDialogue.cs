6b56349b42b79f7e6e1016ef45ae12bfc7837a4d
Viro Neo <van-do.pham@epitech.eu>
2025-01-21 17:31:52 +0100

    :sparkles: added audio for the radio call

--- Assets/Scripts/Dialogue/StartDialogue.cs
+++ Assets/Scripts/Dialogue/StartDialogue.cs
@@ -7,14 +7,23 @@ using Yarn.Unity.Editor;
 
 public class StartDialogue : MonoBehaviour
 {
+    public AudioSource radioAudioSource;
+    public AudioClip radioAnswer;
+    
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
+        {
+            radioAudioSource.Stop();
+            radioAudioSource.clip = radioAnswer;
+            radioAudioSource.Play();
             FindObjectOfType<DialogueRunner>().StartDialogue("Start");
+        }
     }
 
     private void OnCollisionExit(Collision other)
     {
+        radioAudioSource.Stop();
         FindObjectOfType<DialogueRunner>().Stop();
     }
 }
