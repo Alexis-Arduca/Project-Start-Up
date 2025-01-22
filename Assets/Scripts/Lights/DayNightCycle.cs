c9dd2e1f8323a15d93aac6820028050e0f5c1e51
Fizzey <alexis.arduca@epitech.eu>
2025-01-22 14:25:05 +0100

    [ADD] - Post Processing package. Create Sun/Moon cycle
    Post Processing package for light environment. Sun cycle for immersion

---
+++ Assets/Scripts/Lights/DayNightCycle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    private Light sun;
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sun = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sun.transform.Rotate(Vector3.right * speed * Time.deltaTime);
+    }
+}
